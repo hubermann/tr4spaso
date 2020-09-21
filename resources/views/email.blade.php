db465fa390ce898e431e575e41b07c2d9cb9fbce
lorem <hub3r@Hub3rs-MacBook-Pro.local>
2020-09-21 18:25:29 -0300

    normal

--- resources/views/email.blade.php
+++
@@ -1,5 +0,0 @@
-<h3>Mensaje desde el formulario de contacto del website.</h3>
-<p>De: <?= $name ?> </p>
-<p>Asunto: <?=  $subject ?></p>
-<p>Email: <?=  $email ?> Tel: <?= $telephone ?></p>
-<p>Msg: <?= $user_message ?></p>
--- resources/views/new_order_email.blade.php
+++
@@ -1,2 +0,0 @@
-<h3>Una nueva orden a sido pagada en el sistema.</h3>
-<p>Se recibio la aprobacion para la orden: {{ $order_id }}</p>
